6351c44a0fe6c92959d3249750e110ab14a9a495
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-01-25 14:31:23 -0500

    Original Assignment Complete

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,44 +1,49 @@
-
 class TabItem {
   constructor(element) {
-    // attach dom element to object. Example in Tabs class
+    this.element = element; // attach dom element to object. Example in Tabs class
   }
 
   select() {
-    // should use classList
+    this.element.classList.add('Tabs__item-selected') // should use classList
   }
 
   deselect() {
-    // should use classList
+    this.element.classList.remove('Tabs__item-selected'); // should use classList
   }
 }
 
 class TabLink {
   constructor(element, parent) {
-    this.element;// attach dom element to object
-    this.tabs;// attach parent to object
-    this.tabItem;// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
-    // reassign this.tabItem to be a new instance of TabItem, passing it this.tabItem
-    this.element.addEventListener('click', () => {
+    this.element = element; // attach dom element to object
+    this.tabs = parent; // attach parent to object
+    /* Ivan, "This is an example of self-documenting code".  You should do THIS more.  Instead of commenting, you just make the code super easy to read */
+    this.data = this.element.dataset.tab;
+    this.tabItem = this.tabs.getTab(this.data); // assign this to the associated tab using the parent's "getTab" method by passing it the correct data
+    /* What is 'this'??? */
+    this.tabItem = new TabItem(this.tabItem, this); // reassign this.tabItem to be a new instance of TabItem, passing it this.tabItem
+    /* Remember to pass 'event' in as a parameter */
+    this.element.addEventListener('click', (Event) => {
+      event.stopPropagation();
       this.tabs.updateActive(this);
       this.select();
     });
   };
 
   select() {
-    // select this link
-    // select the associated tab
+    /* 'this link' refers to this.tabItem NOT this.tabs */
+    this.element.classList.add("Tabs__link-selected") // select this link
+    this.tabItem.select(); // select the associated tab
   }
 
   deselect() {
-    // deselect this link
-    // deselect the associated tab
+    this.element.classList.remove("Tabs__link-selected"); // deselect this link
+    this.tabItem.deselect(); // deselect the associated tab
   }
 }
 
 class Tabs {
   constructor(element) {
-    this.element = element;// attaches the dom node to the object as "this.element"
+    this.element = element; // attaches the dom node to the object as "this.element"
     this.links = element.querySelectorAll(".Tabs__link");
     this.links = Array.from(this.links).map((link) => {
       return new TabLink(link, this);
@@ -48,16 +53,18 @@ class Tabs {
   }
 
   init() {
-    // select the first link and tab upon ititialization
+    this.activeLink = this.links[0]; // select the first link
+    this.activeLink.select(); // and tab upon intialization
   }
 
   updateActive(newActive) {
-    // deselect the old active link
-    // assign the new active link
+    /* 'the old active link is this.tabItem */
+    this.activeLink.deselect(); // deselect the old active link
+    this.activeLink = newActive; // assign the new active link
   }
 
   getTab(data) {
-    // use the tab item classname and the data attribute to select the proper tab
+    return this.element.querySelector(`.Tabs__item[data-tab="${data}"]`); // use the tab item classname and the data attribute to select the proper tab
   }
 
 }
